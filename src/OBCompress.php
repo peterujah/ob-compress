f17ca393d6c1842507e91c276752eba16af015b0
Chigozie Peter <ujahchigozie@gmail.com>
2023-08-05 06:41:14 +0800

    Update OBCompress.php

--- src/OBCompress.php
+++ src/OBCompress.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * OBCompress - A simple php class to compress output buffer
  * @author      Peter Chigozie(NG) peterujah
@@ -7,7 +7,8 @@
  */
 namespace Peterujah\NanoBlock;
  class OBCompress{
-	/**
+    // Constants for content types
+    /**
 	* holds json content type
 	* @var string JSON
 	*/
@@ -25,273 +26,342 @@ namespace Peterujah\NanoBlock;
 	*/
 	public const HTML = "text/html;";
 
-	/** 
-	* holds content strip setting
-	* @var array OPTIONS
+    /** 
+	* Array to hold response headers
+	* @var array $headers
 	*/
-	private const OPTIONS = array(
-		"find" => array(
-			'/\>[^\S ]+/s',
-			'/[^\S ]+\</s',
-			'/(\s)+/s',
-			'/<!--(.*)-->/Uis',
-			'/[[:blank:]]+/'
-		),
-		"replace" => array(
-			 '>',
-			'<',
-			'\\1',
-			 '',
-			 ' '
-		),
-		"line" => array(
-			"\n",
-			"\r",
-			"\t"
-		)
-	);
-	 
-	/**
-	* holds default server encoding
-	* @var string $encoding
-	*/
-	private $encoding;
-	 
-	/**
-	* holds expected output encoding type
-	* @var string $outputEncoding
-	*/
-	private $outputEncoding;
-	 
-	/** 
-	* holds expected output content type
-	* @var string $$contentType
-	*/
-	private $contentType;
-	
-	/** 
-	* holds gzip stat
+    private $headers;
+
+    /** 
+	*  Gzip compression status
 	* @var bool $gzip
 	*/
-	private $gzip;
-	
-	/** 
-	* holds header expiry time offset
-	* @var string $offset
-	*/
-	private $offset;
+    private $gzip; 
 
 	/** 
-	* holds header cache control
-	* @var string $cacheControl
+	*  ignore User Abort
+	* @var bool $ignoreUserAbort
 	*/
-	private $cacheControl;
-	
+	private $ignoreUserAbort = true;
+
+    private $ignoreCodeblock = false;
+
+	/** 
+	* holds html expiry time offset 7 days
+	* @var string $cacheExpiry
+	*/
+	private $cacheExpiry = 60 * 60 * 24 * 7;
+
+	/** 
+	* holds path to save php to html cached files
+	* @var string $cacheDir
+	*/
+	private $cacheDir = __DIR__ . "/../ob_optimizer_caches";
+
+
+    // Regular expression patterns for content stripping
+    private const OPTIONS = array(
+        "find" => array(
+            '/\>[^\S ]+/s',          // Strip whitespace after HTML tags
+            '/[^\S ]+\</s',          // Strip whitespace before HTML tags
+            '/(\s)+/s',              // Strip excessive whitespace
+            '/<!--(.*)-->/Uis',      // Strip HTML comments
+            '/[[:blank:]]+/'         // Strip blank spaces
+        ),
+        "replace" => array(
+            '>',
+            '<',
+            '\\1',
+            '',
+            ' '
+        ),
+        "line" => array(
+            //'/[\n\r\t]+/'
+            "\n",
+            "\r",
+            "\t"
+        )
+    );
+    
+ 
+    /**
+     * Class constructor.
+     * Initializes default settings for the response headers and cache control.
+     */
+    public function __construct() {
+        $this->headers = array(
+            'Content-Encoding' => '',
+            'Content-Type' => 'charset=utf-8',
+            'Cache-Control' => 'no-store',
+            'Expires' => gmdate("D, d M Y H:i:s", time() + 60 * 60 * 30) . ' GMT',
+            'Content-Length' => 0,
+            'Content-Language' => 'en',
+            'X-Content-Type-Options' => 'nosniff',
+            'X-Frame-Options' => 'deny',
+            'X-XSS-Protection' => '1; mode=block',
+            'Vary' => 'Accept-Encoding',
+            'Connection' => 'close',
+        );
+        $this->gzip = true;
+    }
+
+    /**
+     * Enable or disable Gzip compression.
+     *
+     * @param bool $gzip Enable Gzip compression (true) or disable it (false).
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+    public function useGzip(bool $gzip): OBCompress {
+        $this->gzip = $gzip;
+        return $this;
+    }
+
+    /**
+     * Set the expiration offset for the Cache-Control header.
+     *
+     * @param int $offset Cache expiration offset in seconds.
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+    public function setExpires(int $offset): OBCompress {
+        $this->headers['Expires'] = gmdate("D, d M Y H:i:s", time() + $offset) . ' GMT';
+        return $this;
+    }
 
 	/**
-	* Class constructor
-	*/
-	public function __construct() {
-		if(isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
-			$this->serverEncoding($_SERVER['HTTP_ACCEPT_ENCODING']);
-		}
-		$this->outputEncoding("charset=utf-8");
-		$this->useGzip(true);
-		$this->setExpires(60 * 60 * 30);
-		$this->setCacheControl("must-revalidate");
-	}
+     * Set the expiration offset for the Cache-Control header.
+     *
+     * @param int $expire Cache expiration offset in seconds.
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+    public function setHtmlExpires(int $expire): OBCompress {
+		$this->cacheExpiry = $expire;
+        return $this;
+    }
 
-
-	/**
-	* Sets use gzip state
-	* @param bool $gz enabling gzip
-	* @return OBCompress $this the class instance
-	*/
-	public function useGzip($gz){
-		$this->gzip = $gz;
-		return $this;
-	}
+    /**
+     * Set the Cache-Control header.
+     *
+     * @param string $cacheControl Cache-Control header value.
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+    public function setCacheControl(string $cacheControl): OBCompress {
+        $this->headers['Cache-Control'] = $cacheControl;
+        return $this;
+    }
 
 	/**
-	* Sets the expected expiry offset for header
-	* @param string $off expiry offset
-	* @return OBCompress $this the class instance
-	*/
-	public function setExpires($off){
-		$this->offset = $off;
+     * sets ignore user abort
+     *
+     * @param bool $ignore Cache-Control header value.
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+	public function setIgnoreUserAbort($ignore){
+		$this->ignoreUserAbort = $ignore;
 		return $this;
-	}
-
-	 /**
-	 * Sets the default server content encoding type
-	 * @param string $enc header content encoding
-	 * @return OBCompress $this the class instance
-	 */
-	public function serverEncoding($enc){
-		$this->encoding = $enc;
-		return $this;
-	}
-
-	 /**
-	 * Sets the expected output header content type encoding
-	 * @param string $enc header content type encoding
-	 * @return OBCompress $this the class instance
-	 */
-	public function outputEncoding($enc){
-		$this->outputEncoding = $enc;
-		return $this;
-	}
-
-	 /**
-	 * Sets the expected output header content type
-	 * @param string $ctype header content type
-	 * @return OBCompress $this the class instance
-	 */
-	public function setContentType($ctype){
-		$this->contentType = $ctype;
-		return $this;
-	}
-
-	/**
-	 * Sets the expected output header cache control
-	 * @param string $ctl header cache control type
-	 * @return OBCompress $this the class instance
-	 */
-	public function setCacheControl($ctl){
-		$this->cacheControl = $ctl;
-		return $this;
-	}
-
-
-	 /**
-	 * Compresses the buffer content and added necessary header to optimize the result
-	 * @param string|html|array|text|json $body ob content body
-	 * @param string $type the expected content type to ouput
-	 * @return html|text|json|array|text-gzip-none
-	 */
-
-	 public function compress( $data, $type ) {
-		$content = ($type == self::JSON ? json_encode($data, true) : $data);
-		if ( $this->gzip && !empty($this->encoding) && strpos($this->encoding, 'gzip' ) !== false ) {
-			header( 'Content-Encoding: gzip');
-			$content = gzencode( trim( preg_replace( '/\s+/', ' ', $content ) ), 9);
-		} else {
-			header( "Content-Encoding: none\r\n");
-		}
-		if(!empty($type)){
-			header( "Content-Type: {$type} {$this->outputEncoding}");
-		}else if(!empty($this->contentType)){
-			header( "Content-Type: {$this->contentType}");
-		}
-		header( "Cache-Control: {$this->cacheControl}");
-		header( "expires: " . gmdate("D, d M Y H:i:s", time() + $this->offset) . " GMT" );
-		header( 'Content-Length: ' . ($content != null ? strlen( $content ) : 0) );
-		header( 'Content-Language: en');
-		header( 'X-Content-Type-Options: nosniff');
-		header( 'X-Frame-Options: deny');
-		header( 'X-XSS-Protection: 1; mode=block');
-		header( 'Vary: Accept-Encoding');
-		header( "Connection: close" );
-		return $content;
-	}
-
-	 
-	 /**
-	 * Starts connection and closes it to the browser but continue processing the operation
-	 * @param string|html|array|text|json $body ob content body
-	 * @param int $code the response status code
-	 * @param string $type the expected content type to output
-	 */
-	public function with($body, $code, $type){
-		set_time_limit(0);
-		ignore_user_abort(true);
-		ob_end_clean();
-		echo $this->compress($body, $type);
-		//header("Connection: close");
-		if(!empty($code)){
-			http_response_code($code);
-		}
-		if (ob_get_length() > 0){
-			ob_end_flush();
-			//ob_flush();
-			//flush();
-		}
-	}
-
-	/**
-	* Output output in html format
-	* @param string|html|array|text|json $body content body
-	*/
-	public function html($body){
-		$this->with($body, 200, self::HTML);
-	}
-
-	 /**
-	 * Output output in text format
-	 * @param string|html|array|text|json $body content body
-	 */
-	public function text($body){
-		$this->with($body, 200, self::TEXT);
-	}
-
-	 /**
-	 * Output output in json string format
-	 * @param string|html|array|text|json $body content body
-	 */
-	public function json($body){
-		$this->with($body, 200, self::JSON);
-	}
-
-	 /**
-	 * Output output based on passed content type in second parameter 
-	 * @param string|html|array|text|json $body content body
-	 * @param string $type the expected content type to output
-	 */
-	public function run($body, $type = self::HTML){
-		$this->with($body, 200, $type);
-	}
-	
-	/**
-	 * Ends output buffering 
-	 * @param string $type expected content output type
-	*/
-	public function end($type = self::HTML){
-		$this->with(ob_get_contents(), 200, $type);
 	}
 
     /**
-	 * Strips unwanted tags in document page
-	 * Such as comment and newlines
-	 * @param string|html|array|text|json $buffer content output buffer
-	 * @returns html|text preg_replace
-	 */
-    public static function minify($buffer){
-        return preg_replace(
-			self::OPTIONS["find"], 
-			self::OPTIONS["replace"], 
-			str_replace(
-				self::OPTIONS["line"],
-				'',
-				$buffer
-			)
-		);
-    }
+     * sets ignore user code block
+     *
+     * @param bool $ignore
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+	public function setIgnoreCodeblock($ignore){
+		$this->ignoreCodeblock = $ignore;
+		return $this;
+	}
 
 	/**
-	 * @depreciated
-	 * Strips unwanted tags in document page
-	 * Such as comment and newlines
-	 * @param string|html|array|text|json $buffer content output buffer
-	 * @returns html|text preg_replace
-	 */
-	public static function ob_strip($buffer){
-		return self::minify($buffer);
+     * sets file path
+     *
+     * @param string|dir|path $cacheDir path to save cache
+     * @return OBCompress Returns the class instance for method chaining.
+     */
+	public function setOptimizerCachePath($cacheDir) {
+        $this->cacheDir = $cacheDir;
+		return $this;
     }
 
-	/**
-	 * Starts output buffing and stript unwanted tags in document
-	 */
-	public static function start(){
-		ob_start('self::minify');
-   }
+    /**
+     * Compresses the buffer content and adds necessary headers to optimize the response.
+     *
+     * @param string|array|object $data The content to compress (can be an array or object for JSON response).
+     * @param string $contentType The expected content type for the response.
+     * @return string The compressed content for output.
+     */
+    public function compress($data, string $contentType): string {
+        $content = ($contentType === self::JSON) ? json_encode($data, true) : $data;
+        $minifiedContent = $this->ignoreCodeblock ? self::minifyIgnoreCodeblock($content) : self::minify($content);
+        if ($this->gzip && !empty($_SERVER['HTTP_ACCEPT_ENCODING']) && strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false) {
+            $this->headers['Content-Encoding'] = 'gzip';
+            $minifiedContent = gzencode($minifiedContent, 9);
+        }
+        $this->headers['Content-Length'] = strlen($minifiedContent);
+        $this->headers['Content-Type'] = $contentType . ' ' . $this->headers['Content-Type'];
+
+        foreach ($this->headers as $header => $value) {
+            header("$header: $value");
+        }
+
+        return $minifiedContent;
+    }
+
+    /**
+     * Sends the response with the specified content type and status code.
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     * @param int $statusCode The HTTP status code to be sent in the response.
+     * @param string $contentType The expected content type for the response.
+     */
+    public function with($body, int $statusCode, string $contentType) {
+        set_time_limit(0);
+        ignore_user_abort($this->ignoreUserAbort);
+        ob_end_clean();
+        echo $this->compress($body, $contentType);
+        if ($statusCode) {
+            http_response_code($statusCode);
+        }
+        if (ob_get_length() > 0) {
+            ob_end_flush();
+        }
+    }
+
+    /**
+     * Minify and save output as html, and serve html on next request
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     */
+	public function withOptimizer($body) {
+		$optimizer = new \Peterujah\NanoBlock\OBOptimizer($this->cacheExpiry, $this->cacheDir);
+		$cacheFile = $optimizer->getCacheLocation();
+	
+		if ($optimizer->isCacheValid($cacheFile)) {
+			$optimizer->loadFromCache($cacheFile);
+			return;
+		}
+	
+        $minifiedContent = $this->ignoreCodeblock ? self::minifyIgnoreCodeblock($body) : self::minify($body);
+		set_time_limit(0);
+		ignore_user_abort($this->ignoreUserAbort);
+		echo $optimizer->saveToCache($cacheFile, $minifiedContent);
+		ob_end_flush();
+	}
+
+    /**
+     * Send the output in HTML format.
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     */
+    public function html($body) {
+        $this->with($body, 200, self::HTML);
+    }
+
+    /**
+     * Send the output in text format.
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     */
+    public function text($body) {
+        $this->with($body, 200, self::TEXT);
+    }
+
+    /**
+     * Send the output in JSON format.
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     */
+    public function json($body) {
+        $this->with($body, 200, self::JSON);
+    }
+
+    /**
+     * Send the output based on the specified content type.
+     *
+     * @param string|array|object $body The content body to be sent in the response.
+     * @param string $contentType The expected content type for the response.
+     */
+    public function run($body, string $contentType = self::HTML) {
+        $this->with($body, 200, $contentType);
+    }
+
+    /**
+     * End output buffering and send the response.
+     *
+     * @param string $contentType The expected content type for the response.
+     */
+    public function end(string $contentType = self::HTML) {
+        $this->with(ob_get_contents(), 200, $contentType);
+    }
+
+    /**
+     * Start output buffering and minify the content by removing unwanted tags and whitespace.
+    */
+    public function startMinify() {
+        if($this->ignoreCodeblock){
+            ob_start(['self', 'minifyIgnoreCodeblock']);
+        }else{
+            ob_start(['self', 'minify']);
+        }
+    }
+    
+    /**
+     * Minify the given buffer content by removing unwanted tags and whitespace.
+     *
+     * @param string $buffer The content output buffer to minify.
+     * @return string The minified content.
+     */
+    public static function minify($buffer): string {
+        $minified_buffer = preg_replace(
+            self::OPTIONS["find"],
+            self::OPTIONS["replace"],
+            str_replace(self::OPTIONS["line"], '', $buffer)
+        );
+        return trim(preg_replace('/\s+/', ' ', $minified_buffer));
+    }
+
+    /**
+     * @deprecated This method is deprecated. Use the start() method instead.
+     * Call ob_start(), ob_start(['\Peterujah\NanoBlock\OBCompress', 'minify']); 
+     * or ob_start(['\Peterujah\NanoBlock\OBCompress', 'minifyIgnoreCodeblock']);
+     * Start output buffering and minify the content by removing unwanted tags and whitespace.
+     */
+    public static function start($minify = false) {
+        ob_start($minify ? ['self', 'minify'] : null);
+    }
+    
+    /**
+     * Minify the given buffer content by removing unwanted tags and whitespace.
+     * But ignore html <pre><code> block
+     * @param string $buffer The content output buffer to minify.
+     * @return string The minified content.
+     */
+    public static function minifyIgnoreCodeblock($buffer): string {
+        $ignored_blocks = [];
+        $buffer = preg_replace_callback(
+            '/<pre><code>([\s\S]*?)<\/code><\/pre>/i',
+            function ($matches) use (&$ignored_blocks) {
+                $ignored_blocks[] = $matches[1];
+                return '<!--OB_COMPRESS_IGNORED_BLOCK-->';
+            },
+            $buffer
+        );
+    
+
+        $minified_buffer = preg_replace_callback(
+            '/<!--OB_COMPRESS_IGNORED_BLOCK-->/',
+            function () use (&$ignored_blocks) {
+                $block = array_shift($ignored_blocks);
+                $replacement = preg_replace('/class="language-(.*?)"/i', 'class="$1"', $block);
+                return '<pre><code ' . $replacement . '>' . $block . '</code></pre>';
+            },
+            preg_replace(
+                self::OPTIONS["find"],
+                self::OPTIONS["replace"],
+                $buffer
+            )
+        );
+    
+        return $minified_buffer;
+    }
+    
 }
